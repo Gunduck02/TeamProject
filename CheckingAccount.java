db171cc21c9e2874f4bb92b4b01bbc2744cf47ee
Gunduck2 <kthong5785@gmail.com>
2025-11-12 12:40:14 +0900

    테스트5

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -6,7 +6,7 @@ public class CheckingAccount extends Account {
     }
     public String display() {return null;}
     public boolean withdraw(double amount) {
-        if(amount > 0 && amount <= this.availableBalance) {
+        if(amount > 0 && amount <= this.availableBalance) { //pdf의 출금 조건, 수정 필료
             this.totalBalance -= amount;
             this.availableBalance -= amount;
             return true;
