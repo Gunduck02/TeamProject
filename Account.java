9aade33cebc73f60a7b38a823fe1fe282395a4e2
Gunduck2 <kthong5785@gmail.com>
2025-11-12 11:12:24 +0900

    변경 태스트

--- Account.java
+++ Account.java
@@ -1,7 +1,61 @@
+import java.time.LocalDate;
 
+public abstract class Account {
 
-public class Account {
-    public Account() {
-        // TODO Auto-generated method stub
+    protected Customer owner;
+    protected String accountNumber;
+    protected String accountType;
+    protected double totalBalance;
+    protected double availableBalance;
+    protected LocalDate openDate;
+
+    public Account(Customer owner, String accountNumber, String accountType, double initialDeposit) {
+        this.owner = owner;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.totalBalance = initialDeposit;
+        this.availableBalance = initialDeposit;
+        this.openDate = LocalDate.now();
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.totalBalance += amount;
+            this.availableBalance += amount;
+        } else {
+            //아직 미작성
+        }
+    }
+
+    public abstract boolean withdraw(double amount);
+
+    public abstract String display(); //나중에 당금계좌 저축계좌 구분해서 작성해야하
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public double getTotalBalance() {
+        return totalBalance;
+    }
+
+    public double getAvailableBalance() {
+        return availableBalance;
+    }
+
+    public LocalDate getOpenDate() {
+        return openDate;
     }
 }
--- Customer.java
+++ Customer.java
@@ -1,7 +1,91 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer {
-    public Customer() {
-        // TODO Auto-generated method stub
+
+    private String name;
+    private String customerId;
+    private String password;
+    private String address;
+    private String phone;
+    
+    private List<Account> accountList;
+
+    public Customer(String name, String customerId, String password, String address, String phone) {
+        this.name = name;
+        this.customerId = customerId;
+        this.password = password;
+        this.address = address;
+        this.phone = phone;
+        this.accountList = new ArrayList<>(); 
     }
-}
+
+    public void addAccount(Account account) {
+        this.accountList.add(account);
+    }
+
+    public boolean removeAccount(String accountNumber) {
+        return this.accountList.removeIf(account -> account.getAccountNumber().equals(accountNumber));
+    }
+
+    public Account findAccount(String accountNumber) {
+        for (Account account : this.accountList) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+            }
+        }
+        return null; 
+    }
+
+    public int getNumberOfAccounts() {
+        return this.accountList.size();
+    }
+
+    public double getTotalBalance() {
+        double total = 0.0;
+        for (Account account : this.accountList) {
+            total += account.getTotalBalance();
+        }
+        return total;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public List<Account> getAccountList() {
+        return accountList;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+} //변경내용 테스트
