7acb4ea8151202a36a8ca0f40d376ba1ebe4647f
gunduck2 <kthong5785@gmail.com>
2025-11-12 11:37:04 +0900

    깃허브 테스트 3

--- Customer.java
+++ Customer.java
@@ -20,15 +20,15 @@ public class Customer {
         this.accountList = new ArrayList<>(); 
     }
 
-    public void addAccount(Account account) {
+    public void addAccount(Account account) { //계좌 추가
         this.accountList.add(account);
     }
 
-    public boolean removeAccount(String accountNumber) {
+    public boolean removeAccount(String accountNumber) { //계좌 삭제
         return this.accountList.removeIf(account -> account.getAccountNumber().equals(accountNumber));
     }
 
-    public Account findAccount(String accountNumber) {
+    public Account findAccount(String accountNumber) { //계좌 찾기
         for (Account account : this.accountList) {
             if (account.getAccountNumber().equals(accountNumber)) {
                 return account;
@@ -37,11 +37,11 @@ public class Customer {
         return null; 
     }
 
-    public int getNumberOfAccounts() {
+    public int getNumberOfAccounts() { //계좌 개수
         return this.accountList.size();
     }
 
-    public double getTotalBalance() {
+    public double getTotalBalance() { //전체 잔액
         double total = 0.0;
         for (Account account : this.accountList) {
             total += account.getTotalBalance();
@@ -49,43 +49,43 @@ public class Customer {
         return total;
     }
 
-    public boolean checkPassword(String password) {
+    public boolean checkPassword(String password) { //비밀번호 확인
         return this.password.equals(password);
     }
 
-    public String getName() {
+    public String getName() { //이름
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) {//이름 변경
         this.name = name;
     }
 
-    public String getCustomerId() {
+    public String getCustomerId() { //고객
         return customerId;
     }
 
-    public String getAddress() {
+    public String getAddress() {//주소
         return address;
     }
 
-    public void setAddress(String address) {
+    public void setAddress(String address) { //주소 변경
         this.address = address;
     }
 
-    public String getPhone() {
+    public String getPhone() { //전화번호
         return phone;
     }
 
-    public void setPhone(String phone) {
+    public void setPhone(String phone) { //전화번호 변경
         this.phone = phone;
     }
     
-    public List<Account> getAccountList() {
+    public List<Account> getAccountList() { //계좌 목록 반환
         return accountList;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password) { //비밀번호 변경
         this.password = password;
     }
-} //변경내용 테스트
+} //깃허브 테스트
